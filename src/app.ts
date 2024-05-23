f9386a6cae1504502b18d847f24582af3f131732
DavidChavess <davi.ch.fe@gmail.com>
2024-05-23 18:52:03 -0300

    feat: add route to request and get rides

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
-import express from "express"
+import express, { json } from "express"
 import Signup from "./Signup"
 import { AccountDAODatabase } from "./AccountDAO"
 import GetAccount from "./GetAccount"
+import RequestRide from "./RequestRide"
+import { RideDAODatabase } from "./RideDAO"
+import GetRide from "./GetRide"
 
 const app = express()
 const port = 3000
@@ -11,18 +14,33 @@ app.post('/signup', async (req, res) => {
   try {
     const signup = new Signup(new AccountDAODatabase())
     const response = await signup.execute(req.body)
-    res.status(201).send(response)
+    return res.status(201).json(response)
   } catch (error: any) {
-    res.status(400).send({error: error.message})
+    return res.status(422).json({ message: error.message })
   }
 })
 
 app.get('/signup/:accountId', async (req, res) => {
   const getAccount = new GetAccount(new AccountDAODatabase())
   const response = await getAccount.execute(req.params.accountId)
-  res.json(response)
+  return res.json(response)
 })
 
+app.post('/request_ride', async (req, res) => {
+  try {
+    const requestRide = new RequestRide(new RideDAODatabase(), new AccountDAODatabase())
+    const requestRideOutput = await requestRide.execute(req.body)
+    return res.json(requestRideOutput)
+  } catch (error: any) {
+    return res.status(422).json({ message: error.message })
+  }
+})
+
+app.get('/rides/:rideId', async (req, res) => {
+  const getRide = new GetRide(new RideDAODatabase())
+  const getRideOutput = await getRide.execute(req.params.rideId)
+  return res.json(getRideOutput)
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
--- test/app.test.ts
+++ test/app.test.ts
@@ -1,5 +1,9 @@
 import axios from "axios"
 
+axios.defaults.validateStatus = function () {
+	return true;
+}
+
 test("Deve testar uma conta criada para passageiro", async () => {
   const input = {
     name: 'Fulano Tal',
@@ -17,4 +21,57 @@ test("Deve testar uma conta criada para passageiro", async () => {
   expect(outputGetSignup.name).toBe(input.name)
   expect(outputGetSignup.email).toBe(input.email)
   expect(outputGetSignup.cpf).toBe(input.cpf)
+})
+
+test("Deve testar uma solicitação de corrida feita por um passageiro", async () => {
+  const signupInput = {
+    name: 'Fulano Tal',
+    email: `input${Math.random()}@gmail.com`,
+    cpf: '97456321558',
+    password: 'any password',
+    isPassenger: true
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', signupInput)
+  const outputSignup = responseSignup.data
+  expect(outputSignup.accountId).toBeDefined()
+  const rideInput = {
+    accountId: outputSignup.accountId,
+    fromLat: -21.3750678,
+    fromLong: -48.2409842,
+    toLat: -21.3628963,
+    toLong: -48.2461194
+  }
+  const rideOutputResponse = await axios.post('http://localhost:3000/request_ride', rideInput)
+  const rideOutput = rideOutputResponse.data
+  expect(rideOutput.rideId).toBeDefined()
+  const getRideResponse = await axios.get(`http://localhost:3000/rides/${rideOutput.rideId}`)
+  const getRideOutput = getRideResponse.data
+  expect(getRideOutput.rideId).toBe(rideOutput.rideId)
+  expect(getRideOutput.fromLat).toBe(rideInput.fromLat)
+  expect(getRideOutput.fromLong).toBe(rideInput.fromLong)
+  expect(getRideOutput.toLat).toBe(rideInput.toLat)
+  expect(getRideOutput.toLong).toBe(rideInput.toLong)
+})
+
+test("Não deve solicitar uma corrida por um usuário que não seja passageiro", async () => {
+  const signupInput = {
+    name: 'Fulano Tal',
+    email: `input${Math.random()}@gmail.com`,
+    cpf: '97456321558',
+    password: 'any password',
+    isPassenger: false
+  }
+  const responseSignup = await axios.post('http://localhost:3000/signup', signupInput)
+  const outputSignup = responseSignup.data
+  const rideInput = {
+    accountId: outputSignup.accountId,
+    fromLat: -21.3750678,
+    fromLong: -48.2409842,
+    toLat: -21.3628963,
+    toLong: -48.2461194
+  }
+  const rideOutputResponse = await axios.post('http://localhost:3000/request_ride', rideInput)
+  const rideOutput = rideOutputResponse.data
+  expect(rideOutputResponse.status).toBe(422)
+  expect(rideOutput.message).toBe("A conta não é de um passageiro")
 })
